003f74c55beac0f8c0759fec58c55b8106ffd73b
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2026-03-27 12:40:40 +0600

    Explore gsap svg control

--- raw-gsap/script.js
+++ raw-gsap/script.js
@@ -1,13 +1,25 @@
-gsap.to("#page2 h1", {
-  transform: "translateX(-130%)",
-  duration: 1,
-  scrollTrigger: {
-    trigger: "#page2",
-    scroller: "body",
-    markers: true,
-    scrub: 2,
-    start: "top 0",
-    end: "top -200%",
-    pin: true,
-  },
+let initialPath = `M 10 100 Q 250 100 490 100`;
+let finalPath = `M 10 150 Q 250 200 490 100`;
+
+const string = document.getElementById("string");
+
+string.addEventListener("mousemove", (dets) => {
+  let path = `M 10 100 Q ${dets.x} ${dets.y} 490 100`;
+  gsap.to("svg path", {
+    attr: {
+      d: path,
+    },
+    duration: 0.3,
+    ease: "power3.out",
+  });
 });
+string.addEventListener("mouseleave", () => {
+  gsap.to("svg path", {
+    attr: {
+      d: initialPath,
+    },
+    duration: 0.8,
+    ease: "elastic.out(1,0.2)",
+  });
+});
+string.addEventListener("mouseleave", (e) => {});
